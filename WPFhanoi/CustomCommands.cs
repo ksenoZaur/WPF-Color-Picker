194ed9e076ce9871c7bfed4763754aa2ba1ac096
ksenoZaur <playboyfrombulanikha@gmail.com>
2019-11-29 22:30:02 +0400

    Create WPF Color Pcker

--- WPFhanoi/CustomCommands.cs
+++ WPFhanoi/CustomCommands.cs
@@ -1,7 +1,21 @@
-﻿namespace WPFhanoi
+﻿using System.Windows.Input;
+
+namespace WPFhanoi
 {
-    public class CustomCommands
+    /// <summary>
+    /// Содержит собственные комманды
+    /// </summary>
+    public static class CustomCommands
     {
-        
+        // Зыкрыть приложение
+        public static RoutedUICommand Exit = new RoutedUICommand(
+            "Exit",
+            "Exit",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4, ModifierKeys.Alt)
+            }
+            );
     }
 }
--- WPFhanoi/MainWindow.xaml.cs
+++ WPFhanoi/MainWindow.xaml.cs
@@ -1,13 +1,26 @@
-﻿namespace WPFhanoi
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WPFhanoi
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
     {
-      InitializeComponent();
-    }
-  }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void cmd_Exit(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void cmd_CanExit(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+}
 }
